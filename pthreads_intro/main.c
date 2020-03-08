b49d217d6b10107d1ee7765138640a2b894d1f4c
Anto Sebastian <51168158+Antosebastian@users.noreply.github.com>
2020-03-08 10:46:08 -0400

    Add files via upload

---
+++ pthreads_intro/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* yes(void* var)
+{
+    while(1)
+    {
+        sleep(1);
+        printf("Yes\n");
+    }
+    return NULL;
+}
+
+void no()
+{
+    while(1)
+    {
+        sleep(2);
+        printf("No\n");
+    }
+}
+
+int main()
+{
+    pthread_t yn;
+    pthread_create(&yn, NULL, yes, NULL);
+//    yes();
+    no();
+    return 0;
+}
